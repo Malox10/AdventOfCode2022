1c4c99dd3385c7ed821d82f94cb0959e6049bc9b
Malox <Maxi.Junges@gmx.de>
2022-12-07 16:38:11 +0100

    add template

---
+++ src/main/kotlin/DayTemplate.kt
@@ -0,0 +1,13 @@
+//have to put the function in a class to prevent the IDE from being annoying,
+// it's just here to be copied and pasted anyway
+class DayTemplate {
+    fun main() {
+        val input = readResourceLines("Day.txt")
+        val output = solve(input)
+        println("$output")
+    }
+
+    fun solve(input: List<String>) {
+
+    }
+}
