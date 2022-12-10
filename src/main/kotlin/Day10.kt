9c5b7a97408b8c5855456bae46b3ad6a3ba3589d
Malox <Maxi.Junges@gmx.de>
2022-12-10 18:03:14 +0100

    Day10 Day1

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,51 @@
+fun main() {
+    val input = readResourceLines("Day10.txt")
+    val output = findSignalStrength(input)
+    println("The combined signal strength is: $output")
+}
+
+fun findSignalStrength(input: List<String>): Int {
+    val instructions = input.map { instructionText ->
+        when {
+            instructionText.startsWith("noop") -> Instruction.Noop()
+            instructionText.startsWith("addx") -> {
+                val (_, value) = instructionText.split(" ")
+                Instruction.AddX(value.toInt())
+            }
+            else -> error("Can't parse instruction $instructionText")
+        }
+    }
+
+    val cpu = CPU()
+    instructions.forEach { cpu.doInstruction(it) }
+    return cpu.signalAccumulator
+}
+
+sealed class Instruction() {
+    class Noop: Instruction()
+    data class AddX(val value: Int): Instruction()
+}
+
+class CPU {
+    var cycle: Int = 0
+    var xRegister = 1
+    var signalAccumulator = 0
+
+    private fun finishCycle() {
+        cycle++
+        if(cycle % 40 == 20) {
+            signalAccumulator += xRegister * cycle
+        }
+    }
+
+    fun doInstruction(instruction: Instruction) {
+        when (instruction) {
+            is Instruction.Noop -> finishCycle()
+            is Instruction.AddX -> {
+                finishCycle()
+                finishCycle()
+                xRegister += instruction.value
+            }
+        }
+    }
+}
