1836e8cdf7dc3f528e86fe2546bda51b2aa0f4a0
MaxiJ <Maxi.Junges@gmx.de>
2022-12-21 17:37:55 +0100

    Day15 Part2

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,12 +1,15 @@
 import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
 
 fun main() {
     val input = readResourceLines("Day15.txt")
-    val output = findLocationCountWithNoBeacons(input)
-    println("There are $output tiles without beacons")
+//    val output = findLocationCountWithNoBeacons(input)
+//    println("There are $output tiles without beacons")
+    val output = findDistressBeacon(input)
+    println("The Distress Beacons Signal is: $output")
 }
 
-fun findLocationCountWithNoBeacons(input: List<String>, target: Int = 2000000): Int {
+private fun parseInput(input: List<String>): List<Sensor> {
     val pairs = input.map { line ->
         val(_, sensor, beacon) = line.split("at x=").map { it.replace(":", ", y=") }
         sensor to beacon
@@ -19,14 +22,13 @@ fun findLocationCountWithNoBeacons(input: List<String>, target: Int = 2000000): 
         }
         Sensor(sensor, beacon)
     }
-//    val ranges = sensors.mapNotNull { it.getBlockingRangeAt(10) }
-//    println
 
-    val ranges = sensors.mapNotNull {
-        val range = it.getBlockingRangeAt(target)
-        println("$it, ${it.distance}: $range")
-        range
-    }
+    return sensors
+}
+
+fun findLocationCountWithNoBeacons(input: List<String>, target: Int = 2000000): Int {
+    val sensors = parseInput(input)
+    val ranges = sensors.mapNotNull { it.getBlockingRangeAt(target) }
 
     val simplifiedRanges = ranges.simplify()
     println(simplifiedRanges)
@@ -34,11 +36,28 @@ fun findLocationCountWithNoBeacons(input: List<String>, target: Int = 2000000): 
     return sum - sensors.map { it.closestBeacon }.toSet().count { it.second == target }
 }
 
+const val searchSpace = 4000000
+fun findDistressBeacon(input: List<String>): Long {
+    val sensors = parseInput(input)
+    println(measureTimeMillis {
+        (0..searchSpace).forEach { height ->
+            val ranges = sensors.mapNotNull { it.getBlockingRangeAt(height) }.simplify()
+            if (ranges.isEmpty()) error("range shouldn't be empty")
+            val firstElement = ranges.first()
+            if(ranges.size == 1 && firstElement.start <= 0 && firstElement.end >= searchSpace) return@forEach
+            val firstInBoundRange = ranges.first { it.end > 0 }
+            return (firstInBoundRange.end + 1).toLong() * searchSpace + height
+        }
+    })
+
+    error("no beacon found")
+}
+
 data class Sensor(
     val location: Pair<Int, Int>,
     val closestBeacon: Pair<Int, Int>,
 ) {
-    val distance by lazy {
+    private val distance by lazy {
         val delta = location - closestBeacon
         delta.first.absoluteValue + delta.second.absoluteValue
     }
@@ -56,15 +75,15 @@ data class Sensor(
 
 data class Range(val start: Int, val end: Int)
 
-private fun Iterable<Range>.simplify(): List<Range> {
-    val collection = mutableSetOf<Range>()
+private fun Iterable<Range>.simplify(): Set<Range> {
+    val collection = mutableListOf<Range>()
     val sortedRanges = this.sortedBy { it.start }
 
     var currentRange = sortedRanges.first()
     sortedRanges.drop(1).forEach { nextRange ->
 //        val (firstRange, secondRange) = if(currentRange.start <= nextRange.start) currentRange to nextRange
 //        else nextRange to currentRange
-        if(!(currentRange.contains(nextRange.start) || currentRange.contains(nextRange.end))) {
+        if(!(currentRange.containsOrNextTo(nextRange.start) || currentRange.containsOrNextTo(nextRange.end))) {
             collection.add(currentRange)
             currentRange = nextRange
         }
@@ -76,8 +95,8 @@ private fun Iterable<Range>.simplify(): List<Range> {
     }
 
     collection.add(currentRange)
-    return collection.toList()
+    return collection.toSet()
 }
 
 private operator fun Pair<Int, Int>.minus(other: Pair<Int, Int>) = this.first - other.first to this.second - other.second
-private fun Range.contains(other: Int) = this.start <= other && this.end >= other
+private fun Range.containsOrNextTo(other: Int) = this.start - 1 <= other && this.end + 1 >= other
